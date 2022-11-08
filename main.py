f4b29c29882974d0183f0a3289d0cd727d1a8d15
Oliver Dimes <oliver.dimes3@gmail.com>
2022-11-08 22:12:33 +0000

    Directory Dialog added

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 # This is a PyQT Script
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import sys
 
-if __name__ == "__main__" :
-    app = QApplication([])
-    window = QWidget()
-    layout = QVBoxLayout()
-    layout.addWidget(QPushButton('Top'))
-    layout.addWidget(QPushButton('Bottom'))
-    window.setLayout(layout)
-    window.show()
-    app.exec()
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("PyQT Test : Folder Directory")
+        self.setGeometry(100, 100, 600, 400)
+        self.UiComponents()
+        self.show()
+
+    # method for widgets
+    def UiComponents(self):
+        # creating a push button
+        button = QPushButton("CLICK", self)
+
+        # setting geometry of button
+        button.setGeometry(200, 150, 100, 30)
+
+        # adding action to a button
+        button.clicked.connect(self.pick_new)
+
+    def pick_new(self):
+        dialog = QFileDialog()
+        folder_path = dialog.getExistingDirectory(None, "Select Folder")
+        return folder_path
+
+
+app = QApplication(sys.argv)
+window = Window()
+sys.exit(app.exec())
